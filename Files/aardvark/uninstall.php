fec5162492cca0a3aab04ed319fba2b98e7f6f9b
Steve Pringle <nyhtml@users.noreply.github.com>
2025-08-30 17:38:33 -0400

    uninstall.php
    Create uninstall.php

---
+++ Files/aardvark/uninstall.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Uninstall script for Aardvark Plugin
+ * 
+ * This file is executed when the plugin is deleted from WordPress.
+ * It removes all plugin options from the database.
+ */
+
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    exit; // Prevent direct access
+}
+
+// List of plugin options to remove
+$aardvark_options = [
+    'aardvark_facebook',
+    'aardvark_twitter',
+    'aardvark_linkedin',
+    'aardvark_github',
+    'aardvark_youtube',
+    'aardvark_instagram',
+    'aardvark_enable_phpversion',
+    'aardvark_block_rest_users'
+];
+
+// Delete each option
+foreach ($aardvark_options as $option) {
+    delete_option($option);
+}
+
+// If your plugin stores multisite options, remove them as well
+if (is_multisite()) {
+    global $wpdb;
+    $blog_ids = $wpdb->get_col("SELECT blog_id FROM {$wpdb->blogs}");
+    foreach ($blog_ids as $blog_id) {
+        switch_to_blog($blog_id);
+        foreach ($aardvark_options as $option) {
+            delete_option($option);
+        }
+    }
+    restore_current_blog();
+}
