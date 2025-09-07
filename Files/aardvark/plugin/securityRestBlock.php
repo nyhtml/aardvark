9d55ca0259a5608f3172b0842ca104278026050d
Steve Pringle <nyhtml@users.noreply.github.com>
2025-09-07 12:24:21 -0400

    securityRestBlock.php

--- Files/aardvark/plugin/securityRestBlock.php
+++ Files/aardvark/plugin/securityRestBlock.php
@@ -1,17 +1,28 @@
 <?php
+/*
+Plugin Name: securityRestBlock.php
+Plugin URI: https://github.com/nyhtml/aardvark
+Description: Prevents WordPress REST API user enumeration.
+Version: 1.0.1
+Author: Stephan Pringle
+Author URI: https://www.stephanpringle.com/#aardvark
+Contributors: nyhtml
+Text Domain: aardvark
+License: GPLv3 or later
+License URI: https://www.gnu.org/licenses/gpl.html
+*/
+
 if (!defined('ABSPATH')) exit; // Prevent direct access
 
-// Check if the toggle is enabled
-$block_users_endpoint = get_option('sipylus_block_rest_users', '');
+add_action('rest_api_init', function() {
+    // Correct option key from aardvark.php
+    $block_users_endpoint = filter_var(get_option('aardvark_rest_api_block', false), FILTER_VALIDATE_BOOLEAN);
 
-if ($block_users_endpoint) {
-    add_filter('rest_endpoints', function($endpoints) {
-        if (isset($endpoints['/wp/v2/users'])) {
+    if ($block_users_endpoint) {
+        add_filter('rest_endpoints', function($endpoints) {
             unset($endpoints['/wp/v2/users']);
-        }
-        if (isset($endpoints['/wp/v2/users/(?P<id>[\d]+)'])) {
             unset($endpoints['/wp/v2/users/(?P<id>[\d]+)']);
-        }
-        return $endpoints;
-    });
-}
+            return $endpoints;
+        });
+    }
+});
