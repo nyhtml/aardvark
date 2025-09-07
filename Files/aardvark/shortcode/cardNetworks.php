aa7ca0bf3698dbc3a735afe0b24d077d80cd57ff
Steve Pringle <nyhtml@users.noreply.github.com>
2025-09-07 12:26:52 -0400

    cardNetworks.php

--- Files/aardvark/shortcode/cardNetworks.php
+++ Files/aardvark/shortcode/cardNetworks.php
@@ -1,9 +1,14 @@
 <?php
 /*
-Plugin Name: Sipylus CardNetworks Shortcode
+Plugin Name: cardNetworks.php
 Description: Provides [cardNetworks] shortcode functionality with inline CSS and responsive mobile support.
-Version: 2025.07.13.A
-Author: <a href="http://www.stephanpringle.com">Stephan Pringle</a>
+Version: 1.0.1
+Author: Stephan Pringle
+Author URI: https://www.stephanpringle.com/#aardvark
+Contributors: nyhtml
+Text Domain: aardvark
+License: GPLv3 or later
+License URI: https://www.gnu.org/licenses/gpl.html
 */
 
 if (!defined('ABSPATH')) exit; // Prevent direct access
@@ -17,14 +22,14 @@ function format_social_link($value, $base) {
 
 // SHORTCODE: [cardNetworks]
 function cardnetworks_shortcode() {
-    $facebook  = format_social_link(get_option('sipylus_facebook'),  'https://www.facebook.com/');
-    $twitter   = format_social_link(get_option('sipylus_twitter'),   'https://x.com/');
-    $linkedin  = format_social_link(get_option('sipylus_linkedin'),  'https://linkedin.com/in/');
-    $github    = format_social_link(get_option('sipylus_github'),    'https://github.com/');
-    $instagram = format_social_link(get_option('sipylus_instagram'), 'https://instagram.com/');
+    $facebook  = format_social_link(get_option('aardvark_facebook'),  'https://www.facebook.com/');
+    $twitter   = format_social_link(get_option('aardvark_twitter'),   'https://x.com/');
+    $linkedin  = format_social_link(get_option('aardvark_linkedin'),  'https://linkedin.com/in/');
+    $github    = format_social_link(get_option('aardvark_github'),    'https://github.com/');
+    $instagram = format_social_link(get_option('aardvark_instagram'), 'https://www.instagram.com/');
 
     // Handle YouTube input (supports @handle, full URL, or legacy username)
-    $youtube_input = get_option('sipylus_youtube');
+    $youtube_input = get_option('aardvark_youtube');
     if (strpos($youtube_input, 'http') === 0) {
         $youtube = $youtube_input;
     } elseif (strpos($youtube_input, '@') === 0) {
@@ -44,22 +49,22 @@ function cardnetworks_shortcode() {
     <div class="card-networks-wrapper">
         <div class="card-networks-buttons">
             <?php if ($facebook): ?>
-                <a href="<?php echo esc_url($facebook); ?>" target="_blank" rel="noopener" class="cn-btn facebook">Facebook</a>
+                <a href="<?php echo esc_url($facebook); ?>" target="_blank" rel="nofollow noopener noreferrer" class="cn-btn facebook">Facebook</a>
             <?php endif; ?>
             <?php if ($twitter): ?>
-                <a href="<?php echo esc_url($twitter); ?>" target="_blank" rel="noopener" class="cn-btn twitter">Twitter (X)</a>
+                <a href="<?php echo esc_url($twitter); ?>" target="_blank" rel="nofollow noopener noreferrer" class="cn-btn twitter">Twitter</a>
             <?php endif; ?>
             <?php if ($linkedin): ?>
-                <a href="<?php echo esc_url($linkedin); ?>" target="_blank" rel="noopener" class="cn-btn linkedin">LinkedIn</a>
+                <a href="<?php echo esc_url($linkedin); ?>" target="_blank" rel="nofollow noopener noreferrer" class="cn-btn linkedin">LinkedIn</a>
             <?php endif; ?>
             <?php if ($github): ?>
-                <a href="<?php echo esc_url($github); ?>" target="_blank" rel="noopener" class="cn-btn github">GitHub</a>
+                <a href="<?php echo esc_url($github); ?>" target="_blank" rel="nofollow noopener noreferrer" class="cn-btn github">GitHub</a>
             <?php endif; ?>
             <?php if ($youtube): ?>
-                <a href="<?php echo esc_url($youtube); ?>" target="_blank" rel="noopener" class="cn-btn youtube">YouTube</a>
+                <a href="<?php echo esc_url($youtube); ?>" target="_blank" rel="nofollow noopener noreferrer" class="cn-btn youtube">YouTube</a>
             <?php endif; ?>
             <?php if ($instagram): ?>
-                <a href="<?php echo esc_url($instagram); ?>" target="_blank" rel="noopener" class="cn-btn instagram">Instagram</a>
+                <a href="<?php echo esc_url($instagram); ?>" target="_blank" rel="nofollow noopener noreferrer" class="cn-btn instagram">Instagram</a>
             <?php endif; ?>
         </div>
     </div>
@@ -94,12 +99,13 @@ function cardnetworks_enqueue_styles() {
     .cn-btn:hover {
         opacity: 0.85;
     }
-    .cn-btn.facebook  { background: #3b5998; }
-    .cn-btn.twitter   { background: #1da1f2; }
-    .cn-btn.linkedin  { background: #0077b5; }
-    .cn-btn.github    { background: #333; }
-    .cn-btn.youtube   { background: #ff0000; }
-    .cn-btn.instagram { background: #e1306c; }
+    .cn-btn.facebook    { background: #1877f2; }
+    .cn-btn.twitter     { background: #1da1f2; }
+    .cn-btn.linkedin    { background: #0077b5; }
+    .cn-btn.github      { background: #000000; }
+    .cn-btn.youtube     { background: #ff0000; }
+    .cn-btn.instagram   { background: #e1306c; }
+    .cn-btn.pinterest   { background: #e60023; }
     @media (max-width: 480px) {
         .card-networks-buttons {
             flex-direction: column;
